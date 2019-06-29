554b1d4d341adf7b21d639fe70a1401a17a2bfc3
Mari <mkf346@stern.nyu.edu>
2019-06-28 20:38:21 -0400

    Added to read me  file and added comments to code

--- freestyle.py
+++ freestyle.py
@@ -1,3 +1,4 @@
+#seting up a card dictionary so that each card has a value associated with it
 cards = {
     "2C":2,
     "2D":2,
@@ -53,42 +54,45 @@ cards = {
     "AS":14,
 }
 
+
 import random
-computer = random.choice(cards.keys())
-you = random.choice(cards.keys())
+computer = random.choice(cards.keys())  #defining a variable so that the computer picks a random card from the deck to play
+you = random.choice(cards.keys()) #defining a variable so that "you" pick a random card to play against the computer
 
 print("------------------------")
-print("Computer: " + computer)
-print("You: " + you)
+print("Computer: " + computer)  #tells you what card the computer picked
+print("You: " + you)  #tells you what card you picked
 print("------------------------")
 
 
-if (computer > you):
+if (computer > you): #evaluates if the computer won
     print("Sorry, Computer Wins")
-elif (computer == you):
+elif (computer == you): #evaluates if there is a tie
     print("Tie Game!")
-else:
+else: #if the computer did not win and there is no tie, then you won
     print("You Win!")
 
 print("------------------------")
 
-computer_game_wins = 0
-your_game_wins = 0
-ties = 0
+computer_game_wins = 0 #setting up a count variable to count how many games the computer has won
+your_game_wins = 0  #setting up a count variable to count how many games you won
+ties = 0  #setting up a count variable to count how many ties there have been
 
 for i in cards:
-    if computer > you:
+    if computer > you: #if the computer wins a game, add one game to the count
         computer_game_wins = computer_game_wins + 1
-    if you > computer:
+    if you > computer: #if you win a game, add one game to the count
         your_game_wins = your_game_wins + 1
-    if computer == you:
+    if computer == you:  #if there is a tie, add one game to the count
         ties = ties + 1
-    if computer_game_wins + your_game_wins + ties == 26:
-        print("Game Over")
+    #if computer_game_wins + your_game_wins + ties == 26:  #counts the total number of games played, once you play 26 games, the game is over
+     #   print("Game Over")
 
-print("Number of Computer Wins:" + str(computer_game_wins))
-print("Number of Your Wins:" + str(your_game_wins))
-print("Number of Ties:" + str(ties))
+
+#output: 
+print("Number of Computer Wins:" + str(computer_game_wins)) #tells you how many games the computer has won
+print("Number of Your Wins:" + str(your_game_wins))  #ells you how many games you have won
+print("Number of Ties:" + str(ties))  #tells you how many ties there have been
 print("------------------------")
-print("Total Games Played:" + str(computer_game_wins + your_game_wins + ties))
+print("Total Games Played:" + str(computer_game_wins + your_game_wins + ties))  #tells you how many games have been played so far
 print("------------------------")
